8ed6e4a0bbcd4a8b6b0ee17ed7ab60160d2835ce
Vatsal Mitesh Tailor <37294848+vatsaltailor@users.noreply.github.com>
2024-02-09 16:42:31 -0600

    Update main.cpp

--- util/graph_reader/main.cpp
+++ util/graph_reader/main.cpp
@@ -18,15 +18,10 @@ int main(int args, char **argv)
     
     ginst->BFS(0);
     
-    for(int i = 0; i < ginst->vert_count+1; i++)
-    {
-       int beg = ginst->beg_pos[i];
-       int end = ginst->beg_pos[i+1];
-       std::cout<<i<<"'s neighor list: ";
-       for(int j = beg; j < end; j++)
-           std::cout<<ginst->csr[j]<<" ";
-       std::cout<<"\n";
-    } 
+    // Print BFS traversal path
+    std::cout << "BFS Traversal Path: ";
+    ginst->printBFSPath();
+    
     
     delete ginst;
 
